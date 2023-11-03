0aa910edd8e7f275a24bd9a777c74054e9ab59eb
Temuri Takalandze <takalandzet@gmail.com>
2023-11-03 17:54:36 +0400

    chore(sdk): update API URL

--- packages/sdk/internal/client/api.go
+++ packages/sdk/internal/client/api.go
@@ -8,5 +8,5 @@ import (
 func NewAPIClient(sess *session.Session) *resty.Client {
 	// @todo: set url by ENV.
 	return getBaseClient(sess.Config, sess.Token.AccessToken).
-		SetBaseURL("http://127.0.0.1:8000")
+		SetBaseURL("http://127.0.0.1:8010")
 }
