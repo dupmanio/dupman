9224481466879a554667301b46b14499c77f497e
Temuri Takalandze <takalandzet@gmail.com>
2023-11-04 20:11:05 +0400

    refactor(domain): remove unused errors

--- packages/domain/errors/errors.go
+++ packages/domain/errors/errors.go
@@ -6,7 +6,6 @@ var (
 	ErrUserAlreadyExists           = errors.New("user already exists")
 	ErrUserDoesNotExist            = errors.New("user does not exist")
 	ErrWebsiteNotFound             = errors.New("website not found")
-	ErrAuthorizationRequired       = errors.New("authentication required")
 	ErrAccessIsForbidden           = errors.New("access is forbidden")
 	ErrMissingScopes               = errors.New("missing scopes")
 	ErrUnableToFetchUpdates        = errors.New("unable to fetch Website Updates")
@@ -15,5 +14,4 @@ var (
 	ErrTokenIsExpired              = errors.New("token is expired")
 	ErrUnsupportedNotificationType = errors.New("unsupported notification type")
 	ErrInvalidUserID               = errors.New("user id is invalid or missing")
-	ErrUnableToPublishMessage      = errors.New("unable to publish message")
 )
