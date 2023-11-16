ecf3ed73ad4a9959c8b97e63e1d693b61793c38c
Temuri Takalandze <takalandzet@gmail.com>
2023-11-16 11:10:27 +0400

    refactor(common): refactor gin log wrapper to log handlers structured

--- packages/common/logger/wrapper/gin.go
+++ packages/common/logger/wrapper/gin.go
@@ -1,6 +1,7 @@
 package wrapper
 
 import (
+	"regexp"
 	"strings"
 	"time"
 
@@ -28,14 +29,31 @@ func (wrap *GinWrapper) Write(data []byte) (int, error) {
 
 	switch {
 	case strings.HasPrefix(message, "[GIN-debug]"):
-		wrap.logger.Debug(strings.TrimPrefix(message, "[GIN-debug]"))
+		wrap.logGinDebug(strings.TrimPrefix(message, "[GIN-debug] "))
 	case strings.HasPrefix(message, "[GIN-error]"):
-		wrap.logger.Error(strings.TrimPrefix(message, "[GIN-debug]"))
+		wrap.logger.Error(strings.TrimPrefix(message, "[GIN-debug] "))
 	default:
 		wrap.logger.Info(message)
 	}
 
 	return len(data), nil
+}
+
+func (wrap *GinWrapper) logGinDebug(message string) {
+	re := regexp.MustCompile(`^(\w+)\s+(\S+)\s+-->\s+(\S+)\s+\((\d+)\s+handlers\)$`)
+	if matches := re.FindStringSubmatch(message); len(matches) > 0 {
+		wrap.logger.Debug(
+			"New handler added",
+			zap.String("method", matches[1]),
+			zap.String("route", matches[2]),
+			zap.String("handler", matches[3]),
+			zap.String("handlersNum", matches[4]),
+		)
+
+		return
+	}
+
+	wrap.logger.Debug(message)
 }
 
 func (wrap *GinWrapper) GetGinzapMiddleware() gin.HandlerFunc {
