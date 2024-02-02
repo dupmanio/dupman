99b672717e64ed630663c416ae3bc603665750f3
ABGEO <takalandzet@gmail.com>
2024-02-02 19:10:36 +0400

    feat(dashboard): implement basic OTel integration

--- packages/dashboard/next.config.js
+++ packages/dashboard/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   publicRuntimeConfig: {
     DUPMAN_API: process.env.DUPMAN_API,
   },
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 module.exports = nextConfig;
